a06faa9460472e4e135b9e66431c268cbae21b72
davidfelixvieira1 <david.vieira1526@gmail.com>
2024-07-24 10:21:15 -0400

    changes done

--- week05/scripts/form.js
+++ week05/scripts/form.js
@@ -2,11 +2,11 @@ document.getElementById('currentYear').textContent = new Date().getFullYear();
 document.getElementById('lastModified').textContent = document.lastModified;
 
 const products = [
-    { id: 'fc-1888', name: "flux capacitor", avgRating: 4.5 },
-    { id: 'fc-2050', name: "power laces", avgRating: 4.7 },
-    { id: 'fs-1987', name: "time circuits", avgRating: 3.5 },
-    { id: 'ac-2000', name: "low voltage reactor", avgRating: 3.9 },
-    { id: 'jj-1969', name: "warp equalizer", avgRating: 5.0 }
+    { id: 'fc-1888', name: "Flux capacitor", avgRating: 4.5 },
+    { id: 'fc-2050', name: "Power laces", avgRating: 4.7 },
+    { id: 'fs-1987', name: "Time circuits", avgRating: 3.5 },
+    { id: 'ac-2000', name: "Low voltage reactor", avgRating: 3.9 },
+    { id: 'jj-1969', name: "Warp equalizer", avgRating: 5.0 }
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
